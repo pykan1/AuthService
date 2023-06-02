5a51c7ddb7e62a7d931fdf42d8e0c8c788351753
engi <vitalikmoskov233@gmail.com>
2023-06-02 16:54:03 +0300

    докер филе

--- app/container.py
+++ app/container.py
@@ -3,7 +3,9 @@ from yaml import safe_load
 
 class Container:
     def __init__(self):
-        self.__data = safe_load(open('D:/delivery/backend/AuthService/config.yml', 'r'))
+        #F:/delivery/deliveryBackend/AuthService/config.yml
+        #D:/delivery/backend/AuthService/config.yml
+        self.__data = safe_load(open('F:/delivery/deliveryBackend/AuthService/config.yml', 'r'))
         self.__db = self.__data["db"]
         self.__auth = self.__data["auth"]
 
