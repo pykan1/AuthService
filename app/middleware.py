93623dda25677fc444b7b623b62b168377a538c7
engi <vitalikmoskov233@gmail.com>
2023-06-16 16:25:55 +0300

    добавил в таблицу новый столбец orders

--- app/middleware.py
+++ app/middleware.py
@@ -14,20 +14,20 @@ class Middleware:
 
     def handler_login(self, function):
         def output(*args):
-            person = self._databaseRepository.get_person_by_login(args[1].login)
+            person = self._databaseRepository.get_person_by_login(args[1].login, args[2])
             if person:
                 raise HTTPException(status_code=420, detail="this login is busy")
             else:
-                return function(args[0], args[1])
+                return function(*args)
         return output
 
     def handler_register(self, function):
         def output(*args):
-            person = self._databaseRepository.get_person_by_login(args[1].login)
+            person = self._databaseRepository.get_person_by_login(args[1].login, args[2])
             if not person:
                 raise HTTPException(status_code=419, detail="account not found")
             if not self._passwordRepository.verify_password(args[1].password, person[0].user_password):
                 raise HTTPException(status_code=420, detail="wrong password")
             else:
-                return function(args[0], args[1], person)
+                return function(args[0], args[1], person, args[2])
         return output
